af356575c287172c190a4deacea7dd25cb60b67a
jiaxin luo <jiaxinluo@jiaxindeMacBook-Pro.local>
2021-06-16 13:04:07 +0800

    2021.6.16

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
 /**
  * 请求
+ * query_	--查询
+ * add_		--新增
+ * edit_		--修改
+ * del_		--删除
+ * uoload_	--上传
+ * download_--下载
  */
 import request from '~/plugins/request';
-export const getWeather = data => request({
+export const query_weather = data => request({
 	method:'post',
 	url:'/api/getWeather',
 	data
